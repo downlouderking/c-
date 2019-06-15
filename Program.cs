89d8944dbf8356f570299bc6089443625f23a3f9
a <a@.com>
2019-06-16 02:26:57 +0300

    add

--- Program.cs
+++ Program.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 
-namespace a
+namespace myfirst
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            int q;
+            int q, c;
             q = 1;
+            c = 2;
             Console.WriteLine("q=" + q);
             String a;
             a = "ahmed";
             Console.Write("a =  "+a);
+            
 
         }
     }
