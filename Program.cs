2432684fb3a49ef4993795ebad29cb56a70ce6a8
a <a@.com>
2019-06-16 02:22:06 +0300

    add a comment

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int q;
+            q = 1;
+            Console.WriteLine("q=" + q);
+            String a;
+            a = "ahmed";
+            Console.Write("a =  "+a);
+
+        }
+    }
+}
